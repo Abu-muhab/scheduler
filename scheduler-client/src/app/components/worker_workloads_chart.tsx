98d68fe95e180dbbbb53c1113e4f8c647fdf6eb5
Abdulmalik Abubakar <abumuhab98@gmail.com>
2023-10-29 14:36:46 +0100

    worload chart impl

---
+++ scheduler-client/src/app/components/worker_workloads_chart.tsx
@@ -0,0 +1,39 @@
+import Chart from "react-google-charts";
+
+export default function WorkerWorkloadsChart() {
+    const data = [
+        [
+            "Element",
+            "Density",
+            { role: "style" },
+            {
+                sourceColumn: 0,
+                role: "annotation",
+                type: "string",
+                calc: "stringify",
+            },
+        ],
+        ["Copper", 8.94, "#b87333", null],
+        ["Silver", 10.49, "silver", null],
+        ["Gold", 19.3, "gold", null],
+        ["Platinum", 21.45, "color: #e5e4e2", null],
+    ];
+
+    const options = {
+        title: "Queued items per worker",
+        bar: { groupWidth: "95%" },
+        legend: { position: "none" },
+        vAxis: { title: "Workers" }, // Label for the X-axis
+        hAxis: { title: "Queued items" },
+    };
+
+    return (
+        <Chart
+            chartType="BarChart"
+            width="100%"
+            height="100%"
+            data={data}
+            options={options}
+        />
+    );
+}
--- scheduler-client/src/app/page.tsx
+++ scheduler-client/src/app/page.tsx
@@ -2,47 +2,17 @@
 
 import React from 'react';
 import Workercard from './components/workercard/workercard';
-import { Chart } from "react-google-charts";
+import WorkerTrendChart from './components/worker_trend_chart';
+import WorkerWorkloadsChart from './components/worker_workloads_chart';
 
 
 export default function Dashboard() {
-  const data = [
-    [
-      "Time",
-      "Workers",
-    ],
-    [1, 37.8],
-    [2, 30.9],
-    [3, 25.4],
-    [4, 11.7],
-    [5, 11.9],
-    [6, 8.8],
-    [7, 7.6],
-    [8, 12.3],
-    [9, 16.9],
-    [10, 12.8],
-    [11, 5.3],
-    [12, 6.6],
-    [13, 4.8],
-    [14, 4.2],
-  ];
 
-  const options = {
-    chart: {
-      title: "Time series of number of workers",
-    },
-  };
 
   return <div className='column' style={{ backgroundColor: '#F6F3F3', padding: '20px', gap: '20px' }}>
     <div className='flex-2 row' style={{ gap: '20px' }}>
       <div className='flex-1 card no-elevation' >
-        <Chart
-          chartType="Line"
-          width="100%"
-          height="100%"
-          data={data}
-          options={options}
-        />
+        <WorkerTrendChart />
       </div>
       <div className='flex-1 wrap' style={{ overflow: 'scroll' }}>
         <Workercard width='50%'></Workercard>
@@ -54,7 +24,7 @@ export default function Dashboard() {
       </div>
     </div>
     <div className='flex-2 card no-elevation'>
-
+      <WorkerWorkloadsChart />
     </div>
   </div>
 }
